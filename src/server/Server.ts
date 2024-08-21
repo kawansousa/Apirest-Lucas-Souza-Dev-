7880c46c16ed1778ca2ba562f3ca20dc61e41130
Kawan sousa dos santos <92695689+kawansousa@users.noreply.github.com>
2024-08-21 16:25:57 -0300

    Montagem de pastas

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import 'dotenv/config'
+import { router } from "./routes";
 
 const server = express()
 
+server.use(express.json())
 
-server.get('/', (req, res) => {
-  return res.send('Hello wolrd')
-})
+server.use(router)
 
 export { server }
